6e5da0cfd91cf0012b524ce80178ef143f78198a
Jurquia99 <107203926+Jurquia99@users.noreply.github.com>
2022-07-07 18:37:06 -0400

    Update finalmask.py

--- finalmask.py
+++ finalmask.py
@@ -46,7 +46,7 @@ greatest_area_center_point = center_points[greatest_area_index]
 
 
 #find points on edge of mask with same x center coordinate
-same_center_x = np.where(indicies[1]==greatest_area_center_point[0])
+same_center_x = np.where(indicies[1] == greatest_area_center_point[0])
 
 #find points along same center axis of centerpoint
 center_x_axis = indicies[0][same_center_x]
@@ -54,3 +54,32 @@ center_x_axis = indicies[0][same_center_x]
 midpoint_length = np.size(same_center_x)
 
 num_splits = 3
+
+def draw_vertical_lines(center_axis, mask_border, midpoint_length, num_splits, direction):
+  top_edge_mask = center_x_axis[0]
+  bottom_edge_mask = center_x_axis[-1]
+  for i in range(0, mask_border, midpoint_length_x//num_splits):
+      x_axis =  greatest_area_center_point[0] + (i * direction)
+      strt = (x_axis, top_edge_mask)
+      end = (x_axis, bottom_edge_mask)      
+      cv2.line(img, strt, end, (0, 255, 0), 3)   
+left_verticle_lines = draw_vertical_lines(center_x_axis, border_x, midpoint_length_x, num_splits, -1)
+right_verticle_lines = draw_vertical_lines(center_x_axis, border_x, midpoint_length_x, num_splits, 1)
+
+
+
+same_center_y = np.where(indicies[0] == greatest_area_center_point[1])
+center_y_axis = indicies[1][same_center_y]
+midpoint_length_y = np.size(same_center_y)
+border_y = center_y_axis[-1]
+
+def draw_horizontal_lines(center_axis, mask_border, midpoint_length, num_splits, direction):
+    left_border = 368
+    right_border = 960
+    for i in range(0,mask_border,):
+        y_axis =  greatest_area_center_point[1] + (i * direction)
+        strt = (left_border,y_axis)
+        end = (right_border,y_axis)      
+        cv2.line(img, strt, end, (0, 255, 0), 3)  
+        
+horizontal_lines_up = draw_horizontal_lines(center_y_axis, border_y, midpoint_length_y, num_splits, -1)
